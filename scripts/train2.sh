4a7274fc925826fbdfea87d3467efcb9d7a5c452
HazekiahWon <1470468661@qq.com>
2019-11-29 17:22:06 +0800

    add anno

--- scripts/train2.sh
+++ scripts/train2.sh
@@ -3,12 +3,12 @@ output_dir="./outputs"
 
 ## LR_SCHEDULER_NAME "WarmupMultiStepLR"， "WarmupPolyLR"
 
-export CUDA_VISIBLE_DEVICES=1,2,3
-python tools/train_net2.py --num-gpus 3 --config-file configs/PART-Detection/particle-faster-rcnn-c4.yaml \
+export CUDA_VISIBLE_DEVICES=0,1,2,3
+python tools/train_net2.py --num-gpus 4 --config-file configs/PART-Detection/particle-faster-rcnn-c4.yaml \
        OUTPUT_DIR "$output_dir" \
        SOLVER.OPTIMIZER 'Adam' \
-       SOLVER.IMS_PER_BATCH 6 \
+       SOLVER.IMS_PER_BATCH 8 \
        SOLVER.BASE_LR 0.0005 \
-       SOLVER.MAX_ITER 10000 \
+       SOLVER.MAX_ITER 20000 \
        DATALOADER.NUM_WORKERS 0 \
-       TEST.EVAL_PERIOD 100
+       TEST.EVAL_PERIOD 200
